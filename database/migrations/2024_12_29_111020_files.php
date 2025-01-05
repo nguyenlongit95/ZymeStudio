b9f57b76bb62888ed674482a9ac524fbf2a0147e
LongNguyen <longnguyen@MacBook-Pro-cua-LongNguyen.local>
2025-01-05 17:41:54 +0700

    update migration

--- database/migrations/2024_12_29_111020_files.php
+++ database/migrations/2024_12_29_111020_files.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('role')->default(0);
-            $table->rememberToken();
+            $table->string('filename');
+            $table->timestamp('deadline')->nullable();
+            $table->integer('status')->default(1);
+            $table->integer('priority')->default(1);
+            $table->integer('user_id');
+            $table->integer('synchronize')->default(0);
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
--- database/migrations/2024_12_29_112019_salary.php
+++ database/migrations/2024_12_29_112019_salary.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('salary', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('role')->default(0);
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->integer('status')->default(0);
+            $table->integer('salary')->default(0);
+            $table->timestamp('month');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
